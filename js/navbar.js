9069acc673109fd167d85b59923df7ce15274c1c
Swayam Badhe <123235851+SwayamBadhe@users.noreply.github.com>
2023-09-12 16:22:05 +0530

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,6 @@ window.addEventListener('DOMContentLoaded', function () {
     const navbarDiv = document.getElementById('navbar');
     navbarDiv.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: purple;">
-            <!-- Your navigation bar HTML goes here -->
             <a class="navbar-brand" href="home.html">E-Library</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -25,7 +24,6 @@ window.addEventListener('DOMContentLoaded', function () {
                     <li class="nav-item">
                         <a class="nav-link" href="signup.html">Sign up</a>
                     </li>
-                    <!-- Add links to other pages as needed -->
                 </ul>
             </div>
         </nav>
