aa9234017e77a9a4d69480a35c494c3701d46206
SwayamBadhe <swayampbadhe@gmail.com>
2023-09-20 11:57:58 +0530

    imporved file structure

--- js/navbar.js
+++ js/navbar.js
@@ -3,14 +3,14 @@ window.addEventListener('DOMContentLoaded', function () {
     const navbarDiv = document.getElementById('navbar');
     navbarDiv.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: purple;">
-            <a class="navbar-brand" href="home.html">E-Library</a>
+            <a class="navbar-brand" href="../index.html">E-Library</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="home.html">Home</a>
+                        <a class="nav-link" href="../index.html">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="search.html">Search</a>
